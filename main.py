d2b370488b47364df801604a42cfe3b9316dc2bb
Romaniv Sofia <sofa.romaniv2410@gmail.com>
2024-03-14 15:57:10 +0200

    Innit commit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from star_wars_api import StarWarsApi
+
+api_client = StarWarsApi()
+
+planet = api_client.get_planet(1)
+print(f"Planet name: {planet.name}")
+print(f"rotation_period: {planet.rotation_period}")
+
+people = api_client.get_people(1)
+print(f"People name: {people.name}")
+print(f"height: {people.height}")
+
+starship = api_client.get_starship(1)
+print(f"Starships name: {starship.name}")
+print(f"model: {starship.model}")
+
 
